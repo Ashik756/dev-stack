c90668bacdd1c4de842d8dd8709906f55c06395c
Ashikur Rahaman <ashikurrahaman9988@gmail.com>
2026-09-13 23:15:50 +0600

    Tost bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Suspense } from 'react';
+import { Suspense } from "react"
 import './App.css'
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Technologies, { type TechItem } from "./components/Technologies"
-import TechSkeleton from "./components/TechSkeleton";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import TechSkeleton from "./components/TechSkeleton"
 
 const techPromiseFetch = async (): Promise<TechItem[]> => {
   const res = await fetch("/data.json");
@@ -19,7 +21,6 @@ export default function App() {
     <div>
       <Navbar />
       <Hero />
-
       <Suspense fallback={
         <div className="max-w-7xl mx-auto px-4 py-16">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -31,8 +32,8 @@ export default function App() {
       }>
         <Technologies techPromise={techPromise} />
       </Suspense>
-
       <Footer />
+      <ToastContainer position="bottom-right" autoClose={2000} />
     </div>
   )
 }
